f81bfeb8d4153fbcc801a6231b87915cfdec37f8
Linden <lin07hao@gmail.com>
2022-06-08 01:12:59 +0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
+const appNode = {
+    data(){
+        return {counter:0}
+    },
+    mounted(){
+        setInterval(()=>{this.counter++}, 1000)
+    }
+}
+
+const app = Vue.createApp(appNode).mount('#app');
 
