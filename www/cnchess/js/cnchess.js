81703b3786dfb3cfb79bfee3c45303f6c691491b
Linden <lin07hao@gmail.com>
2022-06-08 22:14:53 +0800

    Newest

---
+++ www/cnchess/js/cnchess.js
@@ -0,0 +1,4 @@
+let red_names = ['車', '馬', '炮', '帥', '仕', '相', '兵']; 
+let black_names = ['車', '馬', '炮', '將', '士', '象', '卒']; 
+for (let i = 0; i < red_names.length; ++i) document.write("<div class='pieces red'>" + red_names[i] + "</div>"); 
+for (let i = 0; i < black_names.length; ++i) document.write("<div class='pieces black'>" + black_names[i] + "</div>"); 
