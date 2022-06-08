5ee38f06478d70cb72824a8dcb93becb48219549
Linden <lin07hao@gmail.com>
2022-06-08 21:41:36 +0800

    Newest

--- js/app.js
+++ js/app.js
@@ -3,9 +3,67 @@ const appNode = {
         return {counter:0}
     },
     mounted(){
-        setInterval(()=>{this.counter++}, 1000)
+        setInterval(()=>{
+            this.counter++;
+            // console.log(this);
+        }, 1000)
     }
 }
 
 const app = Vue.createApp(appNode).mount('#app');
 
+
+const EventHandling = {
+    data() {
+      return {
+        message: 'Hello Vue.js!'
+      }
+    },
+    methods: {
+      reverseMessage() {
+        this.message = this.message
+          .split('')
+          .reverse()
+          .join('')
+      }
+    }
+  }
+  
+  Vue.createApp(EventHandling).mount('#event-handling')
+
+
+  const TwoWayBinding = {
+    data() {
+      return {
+        message: 'Hello Vue!'
+      }
+    }
+  }
+  
+  Vue.createApp(TwoWayBinding).mount('#two-way-binding')
+
+
+  const ConditionalRendering = {
+    data() {
+      return {
+        seen: true
+      }
+    }
+  }
+  
+  Vue.createApp(ConditionalRendering).mount('#conditional-rendering')
+  
+
+  const ListRendering = {
+    data() {
+      return {
+        todos: [
+          { text: 'Learn JavaScript' },
+          { text: 'Learn Vue' },
+          { text: 'Build something awesome' }
+        ]
+      }
+    }
+  }
+  
+  Vue.createApp(ListRendering).mount('#list-rendering')
